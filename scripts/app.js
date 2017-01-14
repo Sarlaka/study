fd518c370e5f8bc7d9909fa620f1baee38e20edc
dcd@163.com <dcd@163.com>
2017-01-14 21:19:20 +0800

    完成热门作者

--- scripts/app.js
+++ scripts/app.js
@@ -4,10 +4,11 @@ Yike.run(['$rootScope',function($rootScope){
 	//设置全局域
 	$rootScope.collapsed = false;
 	$rootScope.loaded = false;
+	$rootScope.title = '今日一刻';
+
 	$rootScope.toggle = function(){
 
 		$rootScope.collapsed = !$rootScope.collapsed;
-
 		var navs = document.querySelectorAll('.navs dd');
 		var len = navs.length;
 		if($rootScope.collapsed){
@@ -32,21 +33,26 @@ Yike.run(['$rootScope',function($rootScope){
 
 	}
 }])
-
+//配置路由
 Yike.config(['$routeProvider',function($routeProvider){
 	$routeProvider.when('/today',{
 		templateUrl : './views/today.html',
 		controller : 'TodayCtrl'
 	}).when('/older',{
-		templateUrl : './views/older.html'
+		templateUrl : './views/older.html',
+		controller : 'OlderCtrl'
 	}).when('/author',{
-		templateUrl : './views/author.html'
+		templateUrl : './views/author.html',
+		controller : 'AuthorCtrl'
 	}).when('/category',{
-		templateUrl : './views/category.html'
+		templateUrl : './views/category.html',
+		controller : 'CategoryCtrl'
 	}).when('/favourite',{
-		templateUrl : './views/favourite.html'
+		templateUrl : './views/favourite.html',
+		controller : 'FavouriteCtrl'
 	}).when('/settings',{
-		templateUrl : './views/settings.html'
+		templateUrl : './views/settings.html',
+		controller : 'SettingsCtrl'
 	}).otherwise({
 		redirectTo : '/today'
 	})
--- scripts/controllers.js
+++ scripts/controllers.js
@@ -3,6 +3,7 @@ angular.module('Ctrls',[])
 .controller('DemoCtrl',['$scope',function($scope){
 
 }])
+//导航栏控制器
 .controller('NavsCtrl',['$scope','$http',function($scope,$http){
 
 	// 通常数据来自后端，假如导航内容固定不变，就没有必要再发请求了，可以用数组进行模拟
@@ -22,13 +23,78 @@ angular.module('Ctrls',[])
 
 	$scope.navs = navs;
 }])
-.controller('TodayCtrl',['$scope','$http','$rootScope',function($scope,$http,$rootScope){
+//今日一刻控制器
+.controller('TodayCtrl',['$scope','$http','$rootScope','$filter',function($scope,$http,$rootScope,$filter){
+	//发起请求之前把loaded改为false
+	$rootScope.loaded = false;
+	$rootScope.title = '今日一刻';
+
+	// 格式化时间
+	var day = new Date;
+	day = $filter('date')(day, 'yyyy-MM-dd');
+	//获得数据
 	$http({
-		url: './interface/today.php'
+		url: './interface/today.php',
+		params: {day:day}
 	}).success(function(data){
 		console.log(data);
 		$scope.posts = data.posts;
 		$scope.date = data.date;
 		$rootScope.loaded = true;
 	})
+}])
+//往期内容控制器
+.controller('OlderCtrl',['$scope','$http','$rootScope',function($scope,$http,$rootScope){
+	//发起请求之前把loaded改为false
+	$rootScope.loaded = false;
+	$rootScope.title = '往期内容';
+
+	$scope.day = -1;
+	// 获得数据
+	$http({
+		url: './interface/older.php',
+		params: {day:$scope.day}
+	}).success(function(data){
+		console.log(data);
+		$scope.posts = data.result.posts;
+		$scope.date = data.result.date;
+
+		$scope.day = data.day;
+
+		$rootScope.loaded = true;
+	})
+}])
+//热门作者
+.controller('AuthorCtrl',['$scope','$http','$rootScope',function($scope,$http,$rootScope){
+	//发起请求之前把loaded改为false
+	$rootScope.loaded = false;
+	$rootScope.title = '热门作者';
+
+	//获得数据
+	$http({
+		url: './interface/author.php'
+	}).success(function(info){
+		console.log(info);
+		// 推荐
+		$scope.rec = info.rec;
+		// 全部
+		$scope.all = info.all;
+
+		$rootScope.loaded = true;
+	})
+}])
+.controller('CategoryCtrl',['$scope','$rootScope',function($scope,$rootScope){
+	//发起请求之前把loaded改为false
+	// $rootScope.loaded = false;
+	$rootScope.title = '栏目浏览';
+}])
+.controller('FavouriteCtrl',['$scope','$rootScope',function($scope,$rootScope){
+	//发起请求之前把loaded改为false
+	// $rootScope.loaded = false;
+	$rootScope.title = '我的喜欢';
+}])
+.controller('SettingsCtrl',['$scope','$rootScope',function($scope,$rootScope){
+	//发起请求之前把loaded改为false
+	// $rootScope.loaded = false;
+	$rootScope.title = '设置';
 }])
